98b8298c44f21c92d2e47d44baff0ed0335cb9aa
Iyin0 <p2iyinoluwa@gmail.com>
2022-06-24 09:25:55 +0100

    first commit

---
+++ info.js
@@ -0,0 +1,21 @@
+const name_cont = document.createElement("h1");
+const name_data = document.createTextNode("Name: Bilewu Iyinoluwa");
+name_cont.appendChild(name_data);
+
+const name_display = document.getElementById("container");
+name_display.appendChild(name_cont);
+
+
+const height_cont = document.createElement("h2");
+const height_data = document.createTextNode("Height: 5'7");
+height_cont.appendChild(height_data);
+
+const height_display = document.getElementById("container");
+height_display.appendChild(height_cont);
+
+const country_cont = document.createElement("h2");
+const country_data = document.createTextNode("Country: Nigeria");
+country_cont.appendChild(country_data);
+
+const country_display = document.getElementById("container");
+country_display.appendChild(country_cont);
